a17a427b5b0a7bfe01d34e816d449fc32b7984c8
Enkhbat Munkhbold <emunkhbold@enkhbats-mbp.attlocal.net>
2019-07-06 22:58:50 -0700

    Added seed.js file and worked on dynamic creating of review elements

--- database/index.js
+++ database/index.js
@@ -5,33 +5,4 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => { console.log('Database is connected!')});
 
-let reviewSchema = mongoose.Schema({
-    picture: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    reviews: {
-        type: Number,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
-    elite: Boolean
-  });
-  
-  let Review = mongoose.model('Review', reviewSchema);
-  let save = ((err, review) => {
-    if(err) { console.error(err);}
-  })
-
-  module.exports.Review = Review;
+module.exports = db;
--- database/seed.js
+++ database/seed.js
@@ -1,1 +1,28 @@
 const db = require('./index.js');
+const _ = require('underscore');
+const Review = require('./reviews.js')
+const faker = require('faker');
+
+let randNum = Math.floor(Math.random() * (6 - 1) + 1);
+let ratingRandNum = Math.floor(Math.random() * 6);
+let eliteRand = Math.floor(Math.random() * 2);
+
+//Creating reviews for a given restaurant
+for(let i = 0; i < randNum; i++) {
+    let arrNums = [];
+
+    random = Math.floor(Math.random() * (50 - 1) + 1);
+    if(!arrNums.includes(random)){
+        arrNums.push(random);
+    }
+
+    Review.create({
+        picture: `https://worldnet-sites.s3-us-west-1.amazonaws.com/images/${arrNums[i]}.jpg`,
+        name: faker.name.findName(),
+        location: faker.address.city() + '. ' + faker.address.state(),
+        reviews: revRandNum,
+        rating: ratingRandNum,
+        elite: eliteRand
+    })
+}
+
