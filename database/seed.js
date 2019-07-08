fa1a9747f75d225073ebd822d7a40baf0e585a78
Enkhbat Munkhbold <emunkhbold@enkhbats-mbp.attlocal.net>
2019-07-08 12:10:04 -0700

    Some refactoring

--- database/seed.js
+++ database/seed.js
@@ -3,28 +3,29 @@ const _ = require('underscore');
 const Review = require('./reviews.js')
 const faker = require('faker');
 
-let randNum = Math.floor(Math.random() * (6 - 1) + 1);
-let ratingRandNum = Math.floor(Math.random() * 6);
-let eliteRand = Math.floor(Math.random() * 2);
-
+let randNumOfReviews = () => { return Math.floor(Math.random() * (6 - 1) + 1) };
+let randomChoice = () => { return Math.floor(Math.random() * 6) };
 
 //Creating reviews for a given restaurant
 const createData = () => {
-    for(let i = 0; i < randNum; i++) {
-        let arrNums = [];
-    
+
+    let arrNums = [];
+    let length = randNumOfReviews();
+    for(let j = 0; j < length; j++) {
         random = Math.floor(Math.random() * (50 - 1) + 1);
         if(!arrNums.includes(random)){
             arrNums.push(random);
         }
-    
+    }
+
+    for(let i = 0; i < length; i++) {
         Review.create({
             picture: `https://worldnet-sites.s3-us-west-1.amazonaws.com/images/${arrNums[i]}.jpg`,
             name: faker.name.findName(),
             location: faker.address.city() + '. ' + faker.address.state(),
-            reviews: randNum,
-            rating: ratingRandNum,
-            elite: eliteRand
+            reviews: randNumOfReviews(),
+            rating: randomChoice(),
+            elite: Math.floor(Math.random() * 2)
         }).then(() => {
             console.log(Review);
         }).catch((err) => {
